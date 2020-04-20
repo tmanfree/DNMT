110efb57978aad78c619fc4fe49262cfe5d582e8
Thomas Mandzie <tmanfree@hotmail.com>
2020-04-20 14:40:24 -0600

    sorting maintenance list

--- DNMT/procedure/statuschecks.py
+++ DNMT/procedure/statuschecks.py
@@ -52,10 +52,10 @@ class StatusChecks:
         #Remove oldest files (listed first on windows
         filelist = os.listdir(os.path.join(self.log_path, "activitycheck", "processedfiles"))
         if len(filelist) > 0 and len(filelist) > maxfiles:
-            self.subs.verbose_printer("##### unsorted list:{} #####".format(filelist))
-            testlist = sorted(filelist)
-            self.subs.verbose_printer("##### sorted list:{} #####".format(testlist))
-            filestoremove = sorted(filelist[0:(len(filelist)-maxfiles)])
+            # self.subs.verbose_printer("##### unsorted list:{} #####".format(filelist))
+            sortedfilelist = sorted(filelist)
+            # self.subs.verbose_printer("##### sorted list:{} #####".format(testlist))
+            filestoremove = sortedfilelist[0:(len(filelist)-maxfiles)]
             self.subs.verbose_printer("total files:{}\nremoving files:{}".format(len(filelist),len(filestoremove)))
             for file in filestoremove:
                 if file.endswith("-FullStatus.csv.zip"):
